e878134dfd2b38a860eb40c333b11b37c4392340
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-11-20 15:54:07 +0300

    создана сущность Author написан файл liquiBase для создания таблицы авторов

---
+++ src/main/java/com/project/book_shop/entity/Author.java
@@ -0,0 +1,35 @@
+package com.project.book_shop.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany
+    private List<Book> books = new ArrayList<>();
+
+}
