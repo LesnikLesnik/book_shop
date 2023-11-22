1180f34024b9485628e03d08d22446ee6894c833
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-11-22 11:51:06 +0300

    Выполнена реализация Автора. Добавлен AuthorController, AuthorRepository, AuthorService (расширен через AuthorServiceImpl)

---
+++ src/main/java/com/project/book_shop/services/impl/AuthorServiceImpl.java
@@ -0,0 +1,39 @@
+package com.project.book_shop.services.impl;
+
+import com.project.book_shop.dto.AuthorDTO;
+import com.project.book_shop.entity.Author;
+import com.project.book_shop.mapper.AuthorMapper;
+import com.project.book_shop.repositories.AuthorRepository;
+import com.project.book_shop.services.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorServiceImpl implements AuthorService {
+    private final AuthorMapper authorMapper;
+    private final AuthorRepository authorRepository;
+
+    @Override
+    public AuthorDTO saveAuthor(AuthorDTO authorDTO) {
+        Author author = authorMapper.toAuthor(authorDTO);
+        author = authorRepository.save(author);
+        return authorMapper.toDto(author);
+    }
+
+    @Override
+    public AuthorDTO getAuthorById(Long id) {
+        Optional<Author> optionalAuthor = authorRepository.findById(id);
+        return optionalAuthor.map(authorMapper::toDto).orElse(null);
+    }
+
+    @Override
+    public List<AuthorDTO> getAllAuthors() {
+        List<Author> authors = authorRepository.findAll();
+        return authors.stream().map(authorMapper::toDto).collect(Collectors.toList());
+    }
+}
