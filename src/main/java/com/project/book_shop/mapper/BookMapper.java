1b3f19ad11f526b42c2b7f4397910b2358ad2435
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-11-22 14:47:54 +0300

    Добавлено 3 книги исправлена ошибка BookMapper Проверено через Postman - все работает

--- src/main/java/com/project/book_shop/mapper/BookMapper.java
+++ src/main/java/com/project/book_shop/mapper/BookMapper.java
@@ -3,9 +3,13 @@ package com.project.book_shop.mapper;
 import com.project.book_shop.dto.BookDTO;
 import com.project.book_shop.entity.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface BookMapper {
-    Book toBook(BookDTO bookDto);
+    @Mapping(target = "authorId", source = "author.id") // Маппим authorId на id автора
     BookDTO toBookDTO(Book book);
+
+    @Mapping(target = "author.id", source = "authorId") // Маппим id автора на authorId
+    Book toBook(BookDTO bookDto);
 }
