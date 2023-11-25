37389d8e4c0666d4007e853ec8fcd92730a1ecc5
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-11-25 14:35:06 +0300

    рефактор

--- src/main/java/com/project/book_shop/repositories/BookRepository.java
+++ src/main/java/com/project/book_shop/repositories/BookRepository.java
@@ -1,6 +1,7 @@
 package com.project.book_shop.repositories;
 
 import com.project.book_shop.entity.Book;
+import com.project.book_shop.repositories.custom.CustomBookRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, CustomBookRepository {
     Optional<Book> findByName(String name);
     Optional<Book> findById(Long id);
 
