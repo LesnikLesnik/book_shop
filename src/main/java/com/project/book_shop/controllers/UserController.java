f993e911078be46a5a6c3adb726426b6a5d9cfe6
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-12-17 17:29:30 +0300

    добавлен эндпоинт аутентификации в UserController Добавлена аутентификация в SecurityConfig

--- src/main/java/com/project/book_shop/controllers/UserController.java
+++ src/main/java/com/project/book_shop/controllers/UserController.java
@@ -1,29 +1,50 @@
 package com.project.book_shop.controllers;
 
+import com.project.book_shop.dto.userDto.SignInDto;
 import com.project.book_shop.dto.userDto.SignUpDto;
 import com.project.book_shop.entity.User;
 import com.project.book_shop.mapper.UserMapper;
+import com.project.book_shop.security.jwt.JwtUtils;
 import com.project.book_shop.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.remote.JMXAuthenticator;
+
 @RestController
-@RequestMapping("/user")
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/signUp")
     public ResponseEntity<Void> registerUser(@RequestBody SignUpDto signUpDto) {
         User newUser = userMapper.signUpDtoToUser(signUpDto);
         userService.register(newUser);
         return ResponseEntity.ok().build();
+    }
+    @PostMapping("/signIn")
+    public ResponseEntity<String> authenticateUser(@RequestBody SignInDto signInDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(signInDto.getLogin(), signInDto.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication.getName());
+
+        return ResponseEntity.ok(jwt);
     }
 
 }
