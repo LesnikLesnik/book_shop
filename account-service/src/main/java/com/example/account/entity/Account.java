f37e1eb669cd81c5ef08f17d9a0e30cbef9a743a
lesnik <kir-suh02@mail.ru>
2024-06-12 20:51:11 +0300

    .

---
+++ account-service/src/main/java/com/example/account/entity/Account.java
@@ -0,0 +1,44 @@
+package com.example.account.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @Column(name = "id")
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="creationDate")
+    private Date creationDate;
+
+    @Column(name="bill_id")
+    private UUID billId;
+
+    @Column(name="books")
+    private List<UUID> books;
+}
