256d4a740ebad157b69bff820d57bd512323c834
lesnik <kir-suh02@mail.ru>
2024-06-16 17:41:31 +0300

    a

--- eureka/src/main/java/com/eureka/EurekaServerApplication.java
+++ eureka/src/main/java/com/eureka/EurekaServerApplication.java
@@ -1,8 +1,7 @@
-package com.example;
+package com.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
