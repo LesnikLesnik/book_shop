8dd99e21d361be628ec7d5d499e4c7fc6ad5a517
lesnik <kir-suh02@mail.ru>
2024-06-19 21:20:32 +0300

    Добавлен swagger во все контроллеры

--- book_service/src/main/java/com/example/book/controller/AuthorController.java
+++ book_service/src/main/java/com/example/book/controller/AuthorController.java
@@ -1,9 +1,12 @@
 package com.example.book.controller;
 
+import com.example.book.controller.annotations.DefaultApiResponses;
 import com.example.book.dto.AuthorRequestDto;
 import com.example.book.dto.AuthorResponseDto;
 import com.example.book.dto.BookResponseDto;
 import com.example.book.service.AuthorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,42 +16,52 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
+@DefaultApiResponses
 @RequiredArgsConstructor
 @RequestMapping("/api/authors")
+@Tag(description = "Author controller", name = "Контроллер авторов")
 public class AuthorController {
 
     private final AuthorService authorService;
 
     @PostMapping("/")
+    @Operation(summary = "Добавление автора")
     public UUID addAuthor(@RequestBody AuthorRequestDto authorRequestDto) {
         return authorService.addAuthor(authorRequestDto);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получение автора по id")
     public AuthorResponseDto getAuthor(@PathVariable UUID id) {
         return authorService.getAuthor(id);
     }
 
+    @GetMapping("/authors")
+    @Operation(summary = "Получение всех авторов по 15 штук на странице")
     public Page<AuthorResponseDto> getAllAuthors(@PageableDefault(size = 15) Pageable pageable) {
         return authorService.getAllAuthors(pageable);
     }
 
     @GetMapping("/author")
+    @Operation(summary = "Получение автора по имени", description = "Name маппится из имени и фамилии")
     public Page<AuthorResponseDto> getAuthorByName(@RequestBody AuthorRequestDto authorRequestDto, Pageable pageable) {
         return authorService.getAuthorByName(authorRequestDto, pageable);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Обновление автора", description = "Не обновляется поле id")
     public AuthorResponseDto updateAuthor(@PathVariable UUID id, @RequestBody AuthorRequestDto authorRequestDto) {
         return authorService.updateAuthor(id, authorRequestDto);
     }
 
     @GetMapping("/{id}/books")
+    @Operation(summary = "Получение всех книг по id автора", description = "Дефолтное кол-во книг на странице - 10")
     public Page<BookResponseDto> getBooksByAuthorId(@PathVariable UUID id, Pageable pageable) {
         return authorService.getBooksByAuthorId(id, pageable);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление автора")
     public void deleteAuthor(@PathVariable UUID id) {
         authorService.deleteAuthor(id);
     }
--- book_service/src/main/java/com/example/book/controller/BookController.java
+++ book_service/src/main/java/com/example/book/controller/BookController.java
@@ -1,9 +1,12 @@
 package com.example.book.controller;
 
+import com.example.book.controller.annotations.DefaultApiResponses;
 import com.example.book.dto.BookForSaleResponseDto;
 import com.example.book.dto.BookRequestDto;
 import com.example.book.dto.BookResponseDto;
 import com.example.book.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,38 +16,47 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
+@DefaultApiResponses
 @RequiredArgsConstructor
 @RequestMapping("/api/books")
+@Tag(description = "Book controller", name = "Контроллер книг")
 public class BookController {
 
     private final BookService bookService;
 
     @PostMapping("/")
+    @Operation(summary = "Создание книги")
     public UUID createBook(@RequestBody BookRequestDto bookRequestDto) {
         return bookService.create(bookRequestDto);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получение книги по id")
     public BookResponseDto getBookById(@PathVariable UUID id) {
         return bookService.getBookById(id);
     }
 
     @GetMapping("/")
+    @Operation(summary = "Получение всех книг")
     public Page<BookResponseDto> getAllBooks(@PageableDefault(size = 15) Pageable pageable) {
         return bookService.getAllBooks(pageable);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Обновление книги", description = "Не обновляется поле id")
     public BookResponseDto update(@PathVariable UUID id, @RequestBody BookRequestDto bookRequestDto) {
         return bookService.update(id, bookRequestDto);
     }
 
     @GetMapping("/sell/{id}")
+    @Operation(summary = "Получение книги для продажи",
+            description = "Получение id и cost, id добавляется в соответствущее поле аккаунта, стоимость списывается со счета")
     public BookForSaleResponseDto getBookForSale(@PathVariable UUID id) {
         return bookService.getBookForSale(id);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление книги")
     public void delete(@PathVariable UUID id) {
         bookService.delete(id);
     }
